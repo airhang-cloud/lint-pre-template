56142b9d10785ff18f83c418ad8076dc1567b48b
Huangmh <huangminhang@fjzzhy.cn>
2022-01-29 14:08:49 +0800

    feat: 同步代码

---
+++ src/components/demo.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>ces</h1>
+</template>
+
+<script>
+export default {
+    name: "index",
+    data() {
+        return {
+            msg: "",
+        };
+    },
+    methods: {
+        handlerWat() {
+            // let nam = 1;
+        },
+    },
+};
+</script>
+
+<style></style>
---
+++ src/components/test.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>
+            dfggfdgsdgdsgdfgfdgdfsggggggggggggggggdsffgdfdfgdfgfdsfgdfgsdsdfgdfgdfgdfgdfhsddhdhhdfhherhrthtrhtrhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh3
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
